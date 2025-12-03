e9e20b590c9ff905b52b13d2cf914ec8c6649ee9
HazarFatihAkman <akman.hazar.fatih@gmail.com>
2025-12-03 09:08:48 +0300

    small changes

--- lua/hazarfatihakman/configs/themes.lua
+++ lua/hazarfatihakman/configs/themes.lua
@@ -1,12 +1,18 @@
 return {
-  {
-    "olivercederborg/poimandres.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("poimandres").setup({  
-      })
-      vim.cmd.colorscheme("poimandres")
-    end,
-  },
+  "rose-pine/neovim",
+  lazy = false,
+  priority = 1000,
+  name = "rose-pine",
+  config = function()
+    require("rose-pine").setup({
+      variant = "main",
+      disable_background = true,
+      disable_float_background = true,
+      styles = {
+        bold = true,
+        italic = true,
+      },
+    })
+    vim.cmd.colorscheme("rose-pine")
+  end,
 }
